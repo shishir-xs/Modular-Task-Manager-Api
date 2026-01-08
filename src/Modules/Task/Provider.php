18f6e5d5c95c8c7e4ae4c4d3a1d1f0b874c0e65e
Shishir Bhuiyan <shishir_b@roxnor.com>
2026-01-08 11:54:07 +0600

    Remove unused service class loading from Provider constructor

--- src/Modules/Task/Provider.php
+++ src/Modules/Task/Provider.php
@@ -8,11 +8,6 @@ class Provider extends AbstractLoader
 {
     public function __construct()
     {
-        // Load service classes (no need to instantiate)
-        $this->classLoader([
-            plugin_dir_path(__FILE__) . 'Services',
-        ]);
-
         // Load and instantiate REST endpoint classes
         $this->loadRESTClasses(
             plugin_dir_path(__FILE__) . 'REST',
