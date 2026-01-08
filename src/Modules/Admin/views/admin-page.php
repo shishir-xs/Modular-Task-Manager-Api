cf3fdf1e50e923461c7066567e0646955ecf850b
Shishir Bhuiyan <shishir_b@roxnor.com>
2026-01-08 16:38:44 +0600

    refactor: Clean up admin page template by removing unnecessary comments

--- src/Modules/Admin/views/admin-page.php
+++ src/Modules/Admin/views/admin-page.php
@@ -1,10 +1,4 @@
-<?php
-/**
- * Admin page template for Task Manager
- */
-
-defined('ABSPATH') || exit;
-?>
+<?php defined('ABSPATH') || exit; ?>
 
 <div class="wrap">
     <h1><?php echo esc_html__('Task Manager', 'modular-task-manager'); ?></h1>
